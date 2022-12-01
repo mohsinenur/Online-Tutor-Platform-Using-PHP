0979465071946c2e0d26a7603d0f0c9ffc6dd093
Srinath <srinathgoud1432@gmail.com>
2022-12-01 20:35:51 +0530

    Update banner.inc.php

--- inc/banner.inc.php
+++ inc/banner.inc.php
@@ -1,2 +1,2 @@
-<h1><a href="index.php">Mohsin<span> Gram</span></a></h1>
-<h3><a href="#">Hotline:018xxxxxxx</a></h3>
+<h1><a href="index.php">Online<span> Tutor</span></a></h1>
+<h3><a href="#">Education</a></h3>
